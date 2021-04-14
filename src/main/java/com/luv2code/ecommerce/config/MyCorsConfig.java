80491bcc65fc0e7a5290bf73ba40d62223851e1f
Roberto <roberto.amato.11.93@gmail.com>
2021-04-14 17:06:03 +0200

    added query in ProductRepository

---
+++ src/main/java/com/luv2code/ecommerce/config/MyCorsConfig.java
@@ -0,0 +1,29 @@
+package com.luv2code.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MyCorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("PUT", "DELETE", "GET", "POST")
+                .allowedHeaders("*")
+                .exposedHeaders("header1", "header2")
+                .allowCredentials(false)
+                .maxAge(3600);
+    }
+
+    @Bean(name = "corsConfigurer")
+    public WebMvcConfigurer getCorsConfigurer() {
+        MyCorsConfig filterCorsConfig = new MyCorsConfig();
+
+        return filterCorsConfig;
+    }
+
+}
--- src/main/java/com/luv2code/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/luv2code/ecommerce/repository/ProductRepository.java
@@ -1,10 +1,16 @@
 package com.luv2code.ecommerce.repository;
 
 import com.luv2code.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RepositoryRestResource(collectionResourceRel = "products", path = "products")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    public Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable page);
+
+    public Page<Product> findByCategoryCategoryNameContaining(@RequestParam("name") String categoryName, Pageable page);
 }
